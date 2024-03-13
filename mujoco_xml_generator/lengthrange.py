5d179a3e934ba50778860cdafca547f7cf24a4de
MasashiSUZUKI <masashi.lateolabrax@gmail.com>
2024-03-13 11:33:54 +0900

    fix: conflict between Enum and _ToString

--- mujoco_xml_generator/lengthrange.py
+++ mujoco_xml_generator/lengthrange.py
@@ -1,9 +1,10 @@
-from interface import _ToString
+from mujoco_xml_generator.interface import _ToString
+from mujoco_xml_generator import utils
 import enum
 
 
 class LengthRange(_ToString):
-    class Mode(enum.Enum, _ToString):
+    class Mode(enum.Enum):
         NoneMode = 0
         Muscle = 1
         MuscleUser = 2
@@ -34,27 +35,28 @@ class LengthRange(_ToString):
             interval: float = 2.0,
             tolrange: float = 0.05
     ):
-        self.mode: str = mode.to_string()
-        self.useexisting: str = str(useexisting).lower()
-        self.uselimit: str = str(uselimit).lower()
-        self.accel: float = accel
-        self.maxforce: float = maxforce
-        self.timeconst: float = timeconst
-        self.timestep: float = timestep
-        self.inttotal: float = inttotal
-        self.interval: float = interval
-        self.tolrange: float = tolrange
+        self.mode = utils.Attribution("mode", mode.to_string())
+        self.useexisting = utils.Attribution("useexisting", useexisting)
+        self.uselimit = utils.Attribution("uselimit", uselimit)
+        self.accel = utils.Attribution("accel", accel)
+        self.maxforce = utils.Attribution("maxforce", maxforce)
+        self.timeconst = utils.Attribution("timeconst", timeconst)
+        self.timestep = utils.Attribution("timestep", timestep)
+        self.inttotal = utils.Attribution("inttotal", inttotal)
+        self.interval = utils.Attribution("interval", interval)
+        self.tolrange = utils.Attribution("tolrange", tolrange)
 
     def to_string(self) -> str:
-        res = "<lengthrange "
-        res += f"mode=\"{self.mode}\" "
-        res += f"useexisting=\"{self.useexisting}\" "
-        res += f"accel={self.accel} "
-        res += f"maxforce={self.maxforce} "
-        res += f"timeconst={self.timeconst} "
-        res += f"timestep={self.timestep} "
-        res += f"inttotal={self.inttotal} "
-        res += f"interval={self.interval} "
-        res += f"tolrange={self.tolrange} "
-        res += "/>"
-        return res
+        attributions = utils.arrange_attributions([
+            self.mode,
+            self.useexisting,
+            self.uselimit,
+            self.accel,
+            self.maxforce,
+            self.timeconst,
+            self.timestep,
+            self.inttotal,
+            self.interval,
+            self.tolrange
+        ])
+        return f"<lengthrange {attributions} />"
