8280fe5e6373d32160665b6cad07b3092d54c80e
MasashiSUZUKI <masashi.lateolabrax@gmail.com>
2024-03-13 16:39:58 +0900

    update: Attribution supporting type.

--- mujoco_xml_generator/utils.py
+++ mujoco_xml_generator/utils.py
@@ -5,13 +5,18 @@ def str_or_none(x) -> str | None:
 
 
 class Attribution:
-    def __init__(self, name: str, value: float | int | bool | tuple[int] | tuple[float] | str | None = None):
+    def __init__(self, name: str, value: float | int | bool | tuple | str | None = None):
         self.name: str = name
 
         if type(value) is bool:
             self.value = str(value).lower()
         elif type(value) is tuple:
-            self.value = " ".join(str(v) for v in value)
+            vs = []
+            for v in value:
+                if type(v) is not int and type(v) is not float:
+                    raise "Specified type is unsupported."
+                vs.append(str(v))
+            self.value = " ".join(vs)
         else:
             self.value = value
 
