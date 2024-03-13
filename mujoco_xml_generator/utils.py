7a2c4afa8434836cc0ec319d9565cc4d7dcb12cc
MasashiSUZUKI <masashi.lateolabrax@gmail.com>
2024-03-13 10:57:50 +0900

    update: Attribution supports boolean type.

--- mujoco_xml_generator/utils.py
+++ mujoco_xml_generator/utils.py
@@ -2,9 +2,11 @@ from .interface import _ToString
 
 
 class Attribution(_ToString):
-    def __init__(self, name: str, value: float | int | str | None = None):
+    def __init__(self, name: str, value: float | int | bool | str | None = None):
         self.name: str = name
         self.value = value
+        if type(self.value) is bool:
+            self.value = str(self.value).lower()
 
     def to_string(self) -> str:
         if self.value is None:
