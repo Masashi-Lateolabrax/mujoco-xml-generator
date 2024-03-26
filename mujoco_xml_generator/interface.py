4ce7cf6e9e9db8131e15126dd19734e268182d63
MasashiSUZUKI <masashi.lateolabrax@gmail.com>
2024-03-27 04:16:38 +0900

    update: GetTypeInterface raises error when not implemented processes.

--- mujoco_xml_generator/interface.py
+++ mujoco_xml_generator/interface.py
@@ -4,11 +4,15 @@ import abc
 class GetTypeInterface(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def __str__(self) -> str:
-        return ""
+        raise NotImplemented
+
+    @abc.abstractmethod
+    def __eq__(self, other) -> bool:
+        raise NotImplemented
 
     @abc.abstractmethod
     def get_type(self) -> str:
-        return ""
+        raise NotImplemented
 
 
 class Orientation(GetTypeInterface, abc.ABC):
