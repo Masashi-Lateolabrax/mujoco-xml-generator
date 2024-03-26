004c74b65e157596696615fe6f0dcfed1a25c7fb
MasashiSUZUKI <masashi.lateolabrax@gmail.com>
2024-03-27 05:14:20 +0900

    test-suc: Joint.

---
+++ tests/test_joint.py
@@ -0,0 +1,20 @@
+from mujoco_xml_generator import common
+from mujoco_xml_generator.body import Joint
+
+
+def test_to_string():
+    sample = Joint()
+    answer = "<joint/>"
+    assert str(sample) == answer
+
+
+def test_to_string_with_none():
+    sample = Joint(
+        name="joint",
+        class_="class",
+        type_=Joint.JointType.HINGE,
+        pos=(0.0, 0.0, 0.0),
+        axis=(1.0, 2.0, 3.0),
+        limited=common.BoolOrAuto.TRUE,
+    )
+    assert str(sample) == "<joint name=\"joint\" class=\"class\" axis=\"1.0 2.0 3.0\" limited=\"true\"/>"
