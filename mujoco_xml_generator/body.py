9bcbb059501073c85df2ca02ae2ab67596d44028
MasashiSUZUKI <masashi.lateolabrax@gmail.com>
2024-03-26 16:12:05 +0900

    impl: Body

---
+++ mujoco_xml_generator/body.py
@@ -0,0 +1,50 @@
+from mujoco_xml_generator import common, utils, interface
+
+from mujoco_xml_generator.body.geom import Geom
+
+
+class Body:
+    SUPPORTED_CHILDREN_TYPES = [Geom]
+
+    def __init__(
+            self,
+            name: str | None = None,
+            childclass: str | None = None,
+            pos: tuple[float, float, float] | None = None,
+            orientation: interface.Orientation = common.Orientation.Quaternion(1, 0, 0, 0),
+            mocap: bool = False,
+            gravcomp: float = 0.0,
+            user: list[float] | None = None
+    ):
+        self.name = utils.Attribution("name", name)
+        self.childclass = utils.Attribution("childclass", childclass)
+        self.pos = utils.Attribution("pos", pos)
+        self.orientation = utils.Attribution(orientation.get_type(), utils.str_or_none(orientation))
+        self.mocap = utils.Attribution("mocap", mocap)
+        self.gravcomp = utils.Attribution("gravcomp", gravcomp)
+        self.user = utils.Attribution("user", user)
+
+        self.children = []
+
+    def add_child(self, child):
+        for t in Body.SUPPORTED_CHILDREN_TYPES:
+            if type(child) is t:
+                self.children.append(child)
+        raise "Unsupported type is added."
+
+    def __str__(self) -> str:
+        attributions = utils.arrange_attributions([
+            self.name,
+            self.childclass,
+            self.pos,
+            self.orientation,
+            self.mocap,
+            self.gravcomp,
+            self.user
+        ])
+
+        return "\n".join([
+            f"<body {attributions}>",
+            "\n".join([str(c) for c in self.children]),
+            "</body>"
+        ])
