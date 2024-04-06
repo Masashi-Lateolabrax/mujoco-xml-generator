83ce0b00c664ba37767ce95220948331c733137e
masashi <masashi.lateolabrax@gmail.com>
2024-04-07 02:49:06 +0900

    update: Generator supported Option.

--- mujoco_xml_generator/generator.py
+++ mujoco_xml_generator/generator.py
@@ -3,10 +3,11 @@ from mujoco_xml_generator import _utils as utils
 from mujoco_xml_generator import WorldBody, Body
 from mujoco_xml_generator import Visual
 from mujoco_xml_generator import Default
+from mujoco_xml_generator import Option
 
 
 class Generator:
-    SUPPORTED_CHILDREN_TYPES = [WorldBody, Body, Visual, Default]
+    SUPPORTED_CHILDREN_TYPES = [WorldBody, Body, Visual, Default, Option]
 
     def __init__(self, model: str | None = "MuJoCo Model"):
         self.model = utils.Attribution("model", model, str, "MuJoCo Model")
