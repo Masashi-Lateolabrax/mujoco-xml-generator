744a5db254d3985be754528122e692b390245f23
mSuzuki <masashi.lateolabrax@gmail.com>
2025-02-17 03:30:42 +0900

    mod: reorganize Compiler

--- mujoco_xml_generator/compiler/compiler.py
+++ mujoco_xml_generator/compiler/compiler.py
@@ -4,32 +4,34 @@ from mujoco_xml_generator import common, _utils as utils
 from mujoco_xml_generator.compiler import LengthRange
 
 
+class LocalOrGlobal(enum.Enum):
+    Local = 0
+    Global = 1
+
+    def __str__(self) -> str:
+        match self:
+            case LocalOrGlobal.Local:
+                return "local"
+            case LocalOrGlobal.Global:
+                return "global"
+        raise "Unexpected error occurred."
+
+
+class AngleType(enum.Enum):
+    Radian = 0
+    Degree = 1
+
+    def __str__(self) -> str:
+        match self:
+            case AngleType.Radian:
+                return "radian"
+            case AngleType.Degree:
+                return "degree"
+        raise "Unexpected error occurred."
+
+
 class Compiler(utils.MuJoCoElement):
     SUPPORTED_CHILDREN_TYPES = [LengthRange]
-
-    class LocalOrGlobal(enum.Enum):
-        Local = 0
-        Global = 1
-
-        def __str__(self) -> str:
-            match self:
-                case Compiler.LocalOrGlobal.Local:
-                    return "local"
-                case Compiler.LocalOrGlobal.Global:
-                    return "global"
-            raise "Unexpected error occurred."
-
-    class AngleType(enum.Enum):
-        Radian = 0
-        Degree = 1
-
-        def __str__(self) -> str:
-            match self:
-                case Compiler.AngleType.Radian:
-                    return "radian"
-                case Compiler.AngleType.Degree:
-                    return "degree"
-            raise "Unexpected error occurred."
 
     def __init__(
             self,
@@ -54,26 +56,26 @@ class Compiler(utils.MuJoCoElement):
             exactmeshinertia: bool = False,
             inertiagrouprange: tuple[int, int] = (0, 5)
     ):
-        self.autolimits = utils.Attribution("autolimits", autolimits)
-        self.boundmass = utils.Attribution("boundmass", boundmass)
-        self.boundinertia = utils.Attribution("boundinertia", boundinertia)
-        self.settotalmass = utils.Attribution("settotalmass", settotalmass)
-        self.balanceinertia = utils.Attribution("balanceinertia", balanceinertia)
-        self.strippath = utils.Attribution("strippath", strippath)
-        self.coordinate = utils.Attribution("coordinate", utils.str_or_none(coordinate))
-        self.angle = utils.Attribution("angle", utils.str_or_none(angle))
-        self.fitaabb = utils.Attribution("fitaabb", fitaabb)
-        self.eulerseq = utils.Attribution("eulerseq", eulerseq)
-        self.meshdir = utils.Attribution("meshdir", meshdir)
-        self.texturedir = utils.Attribution("texturedir", texturedir)
-        self.assetdir = utils.Attribution("assetdir", assetdir)
-        self.discardvisual = utils.Attribution("discardvisual", discardvisual)
-        self.convexhull = utils.Attribution("convexhull", convexhull)
-        self.usethread = utils.Attribution("usethread", usethread)
-        self.fusestatic = utils.Attribution("fusestatic", fusestatic)
-        self.inertiafromgeom = utils.Attribution("inertiafromgeom", utils.str_or_none(inertiafromgeom))
-        self.exactmeshinertia = utils.Attribution("exactmeshinertia", exactmeshinertia)
-        self.inertiagrouprange = utils.Attribution("inertiagrouprange", inertiagrouprange)
+        self.autolimits = utils.Attribution("autolimits", autolimits, bool, True)
+        self.boundmass = utils.Attribution("boundmass", boundmass, float, 0.0)
+        self.boundinertia = utils.Attribution("boundinertia", boundinertia, float, 0.0)
+        self.settotalmass = utils.Attribution("settotalmass", settotalmass, float, -1.0)
+        self.balanceinertia = utils.Attribution("balanceinertia", balanceinertia, bool, False)
+        self.strippath = utils.Attribution("strippath", strippath, bool, False)
+        self.coordinate = utils.Attribution("coordinate", coordinate, str, LocalOrGlobal.Local)
+        self.angle = utils.Attribution("angle", angle, str, AngleType.Degree)
+        self.fitaabb = utils.Attribution("fitaabb", fitaabb, bool, False)
+        self.eulerseq = utils.Attribution("eulerseq", eulerseq, str, "xyz")
+        self.meshdir = utils.Attribution("meshdir", meshdir, str, None)
+        self.texturedir = utils.Attribution("texturedir", texturedir, str, None)
+        self.assetdir = utils.Attribution("assetdir", assetdir, str, None)
+        self.discardvisual = utils.Attribution("discardvisual", discardvisual, bool, False)
+        self.convexhull = utils.Attribution("convexhull", convexhull, bool, True)
+        self.usethread = utils.Attribution("usethread", usethread, bool, True)
+        self.fusestatic = utils.Attribution("fusestatic", fusestatic, bool, False)
+        self.inertiafromgeom = utils.Attribution("inertiafromgeom", inertiafromgeom, str, common.BoolOrAuto.AUTO)
+        self.exactmeshinertia = utils.Attribution("exactmeshinertia", exactmeshinertia, bool, False)
+        self.inertiagrouprange = utils.Attribution("inertiagrouprange", inertiagrouprange, int, (0, 5))
 
         self.children = []
 
